6d5c4e91f3de94c1d5bc4d596c811a46d91d373e
John Esmilio <john.esmilio@edu.sait.ca>
2023-09-21 18:25:43 -0600

    Initial commit

---
+++ app/StudentInfo.js
@@ -0,0 +1,12 @@
+import Link from 'next/link'
+
+export default function StudentInfo() {
+  return (
+    <div>
+      <h2>Name: John Esmilio</h2>
+      <div>Course Section: CPRG 306 D</div>
+      <Link rel="stylesheet" href="https://github.com/john-esmilio">Github Profile</Link>
+    </div>
+  )
+}
+
